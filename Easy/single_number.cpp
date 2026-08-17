ae067cd3fea2fdf89c8844f3edea90cf69db2945
Shruti Konkatwar <shrutikonkatwar@gmail.com>
2026-08-17 10:49:54 +0530

    Add single number solution in single_number.cpp
    Implement a solution to find the single number in an array.

---
+++ Easy/single_number.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+
+        for (int x : nums) {
+            ans ^= x;
+        }
+
+        return ans;
+    }
+};
