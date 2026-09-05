bf7e2cbc8508310028d8ba8f933617939d8c1d6e
Shruti Konkatwar <shrutikonkatwar@gmail.com>
2026-09-05 09:54:26 +0530

    Implement firstStableIndex function in Solution class

---
+++ Medium/Smallest Stable Index.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<long long> prefMax(n);
+        vector<long long> suffMin(n);
+
+        // Prefix maximum
+        prefMax[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefMax[i] = max(prefMax[i - 1], (long long)nums[i]);
+        }
+
+        // Suffix minimum
+        suffMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffMin[i] = min(suffMin[i + 1], (long long)nums[i]);
+        }
+
+        // Find the first stable index
+        for (int i = 0; i < n; i++) {
+            if (prefMax[i] - suffMin[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
