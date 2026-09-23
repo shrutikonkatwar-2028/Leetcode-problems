a048295ba7ed4ae8ad2fd40df56b71feb79648aa
Shruti Konkatwar <shrutikonkatwar@gmail.com>
2026-09-23 17:08:24 +0530

    Implement solution for minimum operations to reduce X

---
+++ Medium/Minimum Operations To Reduce X to Zero.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+
+        long long total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+
+        long long target = total - x;
+
+        // If target is negative, impossible
+        if (target < 0) {
+            return -1;
+        }
+
+        // target = 0 means remove everything
+        if (target == 0) {
+            return n;
+        }
+
+        int left = 0;
+        long long sum = 0;
+        int maxLen = -1;
+
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
+
+            while (sum > target && left <= right) {
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum == target) {
+                maxLen = max(maxLen, right - left + 1);
+            }
+        }
+
+        if (maxLen == -1) {
+            return -1;
+        }
+
+        return n - maxLen;
+    }
+};
